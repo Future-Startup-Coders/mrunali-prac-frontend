7d3d366b859ac2228132d779a204f6dd2f865fdd
Mrunali-bind <bindmrunali@gmail.com>
2024-02-15 17:01:15 +0530

    5dayschallenge

---
+++ 5dayschallenge/day1.2.py
@@ -0,0 +1,5 @@
+arr_num = list(input("Enter numbers : "))
+max_num = max(arr_num)
+index = arr_num.index(max_num)
+print("maximum number is: ", max_num)
+print("Index of maximum number is : ", index)
