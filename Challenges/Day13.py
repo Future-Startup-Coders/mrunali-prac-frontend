d1d233767fb378f9df6574018694e87660d2d716
Mrunali-bind <bindmrunali@gmail.com>
2024-03-05 22:55:36 +0530

    Longest substring without Repeating charach

---
+++ Challenges/Day13.py
@@ -0,0 +1,13 @@
+def lengthOfLongestSubstring(s):
+    d = {}
+    max_length = start = 0
+    for i in range(len(s)):
+        if s[i] in d and start <= d[s[i]]:
+            start = d[s[i]] +1
+        else:
+            max_length = max(max_length, i-start +1)
+        d[s[i]] = i
+    return max_length
+
+s = "abcabcbb"
+print(lengthOfLongestSubstring(s))
