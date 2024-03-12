e66dbea3317ce8e010af18d38675be5521d6a5c7
Mrunali-bind <bindmrunali@gmail.com>
2024-03-12 23:09:05 +0530

    First Bad Version

---
+++ Challenges/Day20.py
@@ -0,0 +1,16 @@
+def firstBadVersion(n):
+    def isBadVersion(version):
+        return version >= bad
+    l = 1
+    r = n
+    while (l<=r):
+        mid = (l+r)//2
+        if isBadVersion(mid):
+            r = mid-1
+        else:
+            l = mid+1
+    return l
+
+n = 5
+bad = 4
+print(firstBadVersion(n))
