640329dbc41413518409177598d8ea88aa607c6e
Mrunali-bind <bindmrunali@gmail.com>
2024-03-15 23:08:07 +0530

    Reverse Only Letters

---
+++ Challenges/Day23.py
@@ -0,0 +1,19 @@
+def reverseOnlyLetters(s):
+    s1='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    l= 0
+    r=len(s)-1
+    s= list(s)
+    while l<r:
+        if s[l] in s1:
+            if s[r] in s1:
+                s[l],s[r]=s[r],s[l]
+                l+=1
+                r-=1
+            else:
+                r-=1
+        else:
+            l+=1
+    return ''.join(s)
+
+s = "ab-cd"
+print(reverseOnlyLetters(s))
