fb5740f8aa3fabdcff730d01ee72035a3271bea5
Mrunali-bind <bindmrunali@gmail.com>
2024-03-21 23:22:39 +0530

    Move Zeroes

---
+++ Challenges/Day29.py
@@ -0,0 +1,10 @@
+def moveZeroes(nums):
+    n = len(nums)
+    i = 0
+    for j in range(n):
+        if (nums[j] != 0):
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+    return nums
+nums = [0,1,0,3,12]
+print(moveZeroes(nums))
